36f2dc4b612ed1b1df4b22fed2797cc00bd13e25
AnaGrau <ana-grau@outlook.com>
2018-01-03 19:13:57 +0200

    Localization in Spanish
    Our program would throw messages in Spanish also

--- src/main/java/com/mycomp/models/HelloWorld.java
+++ src/main/java/com/mycomp/models/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World! (in English)");
+        System.out.println("¡Hola Mundo! (in Spanish)");
     }
 }
